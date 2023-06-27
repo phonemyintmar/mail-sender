00fe3fc1df2211bc498a6a6378c1e6bc94eaf5cf
phonemyintmarr@gmail.com <phonemyintmar@mytel.com.mm>
2023-06-27 14:14:45 +0630

    added listener

--- src/main/java/com/pm/mailsender/kafka/Listener.java
+++ src/main/java/com/pm/mailsender/kafka/Listener.java
@@ -1,5 +1,7 @@
 package com.pm.mailsender.kafka;
 
+import com.pm.mailsender.mail.MailPayload;
+import com.pm.mailsender.mail.Sender;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -9,8 +11,18 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class Listener {
 
-    @KafkaListener()
-    public void onReceive(ConsumerRecord<String,Object> record){
+    private final Sender sender;
 
+    public Listener(Sender sender) {
+        this.sender = sender;
+    }
+
+    @KafkaListener(topics = "${listener.config.topic}", groupId = "${listener.config.groupId}")
+    public void onReceive(ConsumerRecord<String, MailPayload> record) {
+        // d mhr clarify payload.
+        MailPayload mailPayload = record.value();
+
+        //log ml so d mhr sa log, check recordId and status and blah blah
+        sender.onSend(mailPayload);
     }
 }
